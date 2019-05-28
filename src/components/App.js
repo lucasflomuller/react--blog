ac9264b39f125aa53c86d0687808113a1211fec0
Lucas Muller <lucasflorianomuller@gmail.com>
2019-05-28 16:34:11 -0300

    add postlist boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PostList from './PostList';
 
 class App extends Component {
   render() { 
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <PostList />
+      </div>
+    );
   }
 }
- 
+
 export default App;
